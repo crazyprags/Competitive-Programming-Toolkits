c83672bdefa78be2ffd5f18a6b1a58fa553b3833
Pragyayan Guha Roy <pgroy.ee.ug@jadavpuruniversity.in>
2025-06-09 18:30:41 +0530

    Modular Exponentiation Template

--- trial.java
+++ trial.java
@@ -8,18 +8,14 @@ public class trial
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static boolean sieve[];
-    public static void sieveOfEratosthenes(int n)
+    static int MOD = (int)1e9+7;
+    int power(int a, int b)
     {
-        sieve = new boolean[n+1];
-        Arrays.fill(sieve, true);
-        sieve[0] = sieve[1] = false;
-        for(int i=2; i<=n; i++)
-        {
-            if(sieve[i])
-            {
-                for(int j=i*i; j<=n; j+=i)  sieve[j] = false;
-            }
-        }
+        if(b==0)    return 1;
+        a%=MOD;
+        int half_power = power(a, b/2);
+        if(b%2==0)  return (half_power*half_power)%MOD;
+        else return (half_power*half_power*a)%MOD;
     }
     public static void main(String args[])
     {
